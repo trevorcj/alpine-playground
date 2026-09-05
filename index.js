699e1237c6afc0b3b59848cb30a30b596c32851c
Trevor C. Justus <trevorcjustus@gmail.com>
2026-09-05 23:32:10 +0100

    form reset and validation

--- index.js
+++ index.js
@@ -7,15 +7,49 @@ document.addEventListener("alpine:init", () => {
     bio: "",
     newsletter: true,
     showPassword: false,
+    errors: {},
 
-    submitForm() {
-      console.log({
-        username: this.username,
-        password: this.password,
-        belt: this.belt,
-        bio: this.bio,
-        newsletter: this.newsletter,
-      });
+    validateForm() {
+      this.errors = {};
+
+      if (this.username.length < 3) {
+        this.errors.username = "Username must be at least 3 characters.";
+      }
+      if (this.password.length < 6) {
+        this.errors.password = "Password must be at least 6 characters.";
+      }
+      if (this.password !== this.passwordConfirm) {
+        this.errors.passwordConfirm = "Passwords do not match.";
+      }
+      if (!this.belt) {
+        this.errors.belt = "Please select a belt color.";
+      }
+      if (this.bio.length < 10) {
+        this.errors.bio = "Bio must be at least 10 characters.";
+      }
+    },
+
+    submitForm($event) {
+      this.validateForm();
+      console.log(this.errors);
+
+      if (Object.keys(this.errors).length === 0) {
+        console.log({
+          username: this.username,
+          password: this.password,
+          belt: this.belt,
+          bio: this.bio,
+          newsletter: this.newsletter,
+        });
+
+        // this.username = "";
+        // this.password = "";
+        // this.passwordConfirm = "";
+        // this.belt = "";
+        // this.bio = "";
+
+        $event.target.reset();
+      }
     },
   }));
 });
